514ebc3d6089ce777c447245651719f2ab0867ee
Mutuba <daniel.mutuba@andela.com>
2019-10-17 10:25:44 +0300

    Implement elastic search for rails

---
+++ db/migrate/20191016103112_create_posts.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :author
+      t.string :title
+      t.text :body
+      t.string :tags
+      t.boolean :published
+      t.timestamp :published_on
+
+      t.timestamps
+    end
+  end
+end
